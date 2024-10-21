836397e183911536d3a3337bbb4c77adbd237f9a
notlim_dev <elmotigre6@gmail.com>
2024-10-21 04:30:46 -0500

    Creando el controlador para crear project

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,7 +1,21 @@
 import type { Response,Request } from "express"
+import Project from "../models/Project"
 
 export class ProjectController {
+
+  static createProject = async (req: Request, res:Response) => {
+      
+    const project = new Project(req.body) 
+    try {
+      await project.save()
+      res.json({msg:'Proyecto Creado correctamente'})
+    } catch (error) {
+     console.log(error);
+      
+    }
+  }
+
   static getAllProjects = async (req: Request, res:Response) => {
-      res.json({msg:'hola'})
+      res.json({msg:'listado todos los proyectos'})
   }
 }
