bf000bf24e87bb863e07018807c45819bafbd2b3
notlim_dev <elmotigre6@gmail.com>
2024-10-21 18:45:57 -0500

    Validacion Para la ruta PUT - Actualizar un proyecto

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -13,12 +13,27 @@ router.post('/',
   body('description')
     .notEmpty().withMessage('El nombre de la descripcion es Obligatorio'),
   handleImputErrors,
-  ProjectController.createProject)
+  ProjectController.createProject
+)
+
 router.get('/', ProjectController.getAllProjects)
 
 router.get('/:id',
   param('id').isMongoId().withMessage('ID no válido'),
   handleImputErrors,
-  ProjectController.getProjectById)
+  ProjectController.getProjectById
+)
+
+router.put('/:id',
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es Obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es Obligatorio'),
+  body('description')
+    .notEmpty().withMessage('El nombre de la descripcion es Obligatorio'),
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleImputErrors,
+  ProjectController.updateProject
+)
 
 export default router
