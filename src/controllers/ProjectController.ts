a0daa6d5aecbb05d7ca4c59e8d61469238e73ab2
notlim_dev <elmotigre6@gmail.com>
2024-10-21 18:01:26 -0500

    filtrado de datos en el controlador hacia la base de datos.

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -1,28 +1,46 @@
-import type { Response,Request } from "express"
+import type { Response, Request } from "express"
 import Project from "../models/Project"
 
 export class ProjectController {
 
-  static createProject = async (req: Request, res:Response) => {
-      
-    const project = new Project(req.body) 
+  static createProject = async (req: Request, res: Response) => {
+
+    const project = new Project(req.body)
     try {
       await project.save()
-      res.json({msg:'Proyecto Creado correctamente'})
+      res.json({ msg: 'Proyecto Creado correctamente' })
     } catch (error) {
-     console.log(error);
-      
+      console.log(error);
+
     }
   }
 
-  static getAllProjects = async (req: Request, res:Response) => {
+  static getAllProjects = async (req: Request, res: Response) => {
     try {
       const projects = await Project.find({})
       res.json(projects)
     } catch (error) {
       console.log(error);
-      
+
     }
-    res.json({msg:'listado todos los proyectos'})
+
+  }
+  static getProjectById = async (req: Request, res: Response) => {
+
+    const { id } = req.params
+    try {
+      const project = await Project.findById(id)
+
+      if (!project) {
+        const error = new Error('Proyecto no Encontrado')
+        res.status(404).json({ error: error.message })
+        return
+      }
+      res.json(project)
+    } catch (error) {
+      console.log(error);
+
+    }
+
   }
 }
