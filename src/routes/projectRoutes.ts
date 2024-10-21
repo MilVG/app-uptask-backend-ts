8147c458cba8ee00395d9f451e5a692133e637b7
notlim_dev <elmotigre6@gmail.com>
2024-10-21 11:47:04 -0500

    Incorporando validaciones para los campos de datos en las Rutas.

--- src/routes/projectRoutes.ts
+++ src/routes/projectRoutes.ts
@@ -1,9 +1,19 @@
 import { Router } from "express";
+import { body } from "express-validator";
 import { ProjectController } from "../controllers/ProjectController";
+import { handleImputErrors } from "../middleware/validation";
 
 const router = Router()
 
-router.post('/',ProjectController.createProject)
+router.post('/',
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es Obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es Obligatorio'),
+  body('description')
+    .notEmpty().withMessage('El nombre de la descripcion es Obligatorio'),
+  handleImputErrors,
+  ProjectController.createProject)
 router.get('/',ProjectController.getAllProjects)
 
 export default router
