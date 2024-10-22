8177802f715c2ee508da2fecc3000e1a5460b792
notlim_dev <elmotigre6@gmail.com>
2024-10-21 22:15:37 -0500

    Finalziacion del capitulo 2 modelos-rutas-controladores

---
+++ src/routes/projectRoutes.ts
@@ -0,0 +1,43 @@
+import { Router } from "express";
+import { body, param } from "express-validator";
+import { ProjectController } from "../controllers/ProjectController";
+import { handleImputErrors } from "../middleware/validation";
+
+const router = Router()
+
+router.post('/',
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es Obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es Obligatorio'),
+  body('description')
+    .notEmpty().withMessage('El nombre de la descripcion es Obligatorio'),
+  handleImputErrors,
+  ProjectController.createProject
+)
+
+router.get('/', ProjectController.getAllProjects)
+
+router.get('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleImputErrors,
+  ProjectController.getProjectById
+)
+
+router.put('/:id',
+  body('projectName')
+    .notEmpty().withMessage('El nombre del proyecto es Obligatorio'),
+  body('clientName')
+    .notEmpty().withMessage('El nombre del cliente es Obligatorio'),
+  body('description')
+    .notEmpty().withMessage('El nombre de la descripcion es Obligatorio'),
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleImputErrors,
+  ProjectController.updateProject
+)
+router.delete('/:id',
+  param('id').isMongoId().withMessage('ID no válido'),
+  handleImputErrors,
+  ProjectController.deleteProject
+)
+export default router
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
 import express  from "express";
 import dotenv from 'dotenv'
 import { connectDB } from "./config/db";
-
+import ProjectRoutes from "./routes/projectRoutes"
 dotenv.config()
 
 connectDB()
 const app = express()
+
+//agregando lectura en la consola de json
+app.use(express.json())
+
+//Routes
+app.use('/api/projects',ProjectRoutes)
 
 export default app;
