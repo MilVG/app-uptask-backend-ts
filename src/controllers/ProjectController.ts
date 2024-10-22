affa9373d632fb9204195bf94c7ead89b944a65b
notlim_dev <elmotigre6@gmail.com>
2024-10-21 19:14:45 -0500

    Implementacion envio de datos controlador DELETE

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -63,4 +63,24 @@ export class ProjectController {
     }
 
   }
+  static deleteProject = async (req: Request, res: Response) => {
+
+    const { id } = req.params
+    try {
+      const project = await Project.findById(id)
+
+      if (!project) {
+        const error = new Error('Proyecto no Encontrado')
+        res.status(404).json({ error: error.message })
+        return
+      }
+      await project.deleteOne()
+      res.json({ msg: 'proyecto Eliminado' })
+    } catch (error) {
+      console.log(error);
+
+    }
+
+  }
+
 }
