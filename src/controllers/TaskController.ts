fe8e74c2bb420faf28d3d4f18c997b8c984b95d5
notlim_dev <elmotigre6@gmail.com>
2024-10-23 14:35:49 -0500

    Filtrar una Terea especifica - Endpoint_GET

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -23,4 +23,26 @@ export class TaskController {
     }
   }
 
+  static getProjectTaskById = async (req: Request, res: Response) => {
+
+    try {
+      const { taskId } = req.params
+      const task = await Task.findById(taskId)
+      if (!task) {
+        const error = new Error('Tarea no encontrada')
+        res.status(404).json({ error: error.message })
+        return
+      }
+      if (task.project.toString() !== req.project.id) {
+        const error = new Error('Acción no válida')
+        res.status(404).json({ error: error.message })
+        return
+      }
+
+      res.json(task)
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' })
+      return
+    }
+  }
 }
