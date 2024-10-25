9675a51e1156d61c03b898d9a3f76117ab2fdf7b
notlim_dev <elmotigre6@gmail.com>
2024-10-24 22:05:58 -0500

    Configuracion - Permitir acceso de url externa cliente mediante cors.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express  from "express";
 import dotenv from 'dotenv'
+import cors from 'cors'
+import { corsConfig } from "./config/cors";
 import { connectDB } from "./config/db";
 import ProjectRoutes from "./routes/projectRoutes"
 dotenv.config()
 
 connectDB()
 const app = express()
+app.use(cors(corsConfig))
 
 //agregando lectura en la consola de json
 app.use(express.json())
