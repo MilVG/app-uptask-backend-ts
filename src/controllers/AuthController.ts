3f059efa12cfb050303bc3ba32dad9cc0a161914
notlim_dev <elmotigre6@gmail.com>
2024-10-31 22:54:30 -0500

    Incorporamos en nuestro registro de usuario el hasheo de password.

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -1,12 +1,17 @@
 import type { Request, Response } from "express"
 import User from "../models/User"
+import { hashPassword } from "../utils/auth"
 export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     try {
+      const { password } = req.body
       const user = new User(req.body)
+      //Hash Paswword
+      user.password = await hashPassword(password)
       await user.save()
       res.json({ msg: 'usuario Creado correctamente' })
+
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' })
     }
