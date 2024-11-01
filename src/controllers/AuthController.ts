d74bf324907e130775594d034c5ce6f8df66b3cd
notlim_dev <elmotigre6@gmail.com>
2024-10-31 23:11:12 -0500

    Prevenir Usuarios Duplicados - validacion  Mediante el email

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -5,8 +5,19 @@ export class AuthController {
 
   static createAccount = async (req: Request, res: Response) => {
     try {
-      const { password } = req.body
+
+      const { password, email } = req.body
+
+      //Prevenir usuarios duplicados
+      const userExists = await User.findOne({ email })
+      if (userExists) {
+        const error = new Error('El Usuario ya está registrado')
+        return res.status(409).json({ error: error.message })
+      }
+
+      //Crear Usuario
       const user = new User(req.body)
+
       //Hash Paswword
       user.password = await hashPassword(password)
       await user.save()
