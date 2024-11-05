6253714156a66a9362e77519d85d3db017699397
notlim_dev <elmotigre6@gmail.com>
2024-11-05 15:29:04 -0500

    Agremaos el usuario al crear crear el proyecto
    => el req.user.id viene el middleware de autentcacion previemnete
    verificado de que exista esta seguridad

--- src/controllers/ProjectController.ts
+++ src/controllers/ProjectController.ts
@@ -6,6 +6,8 @@ export class ProjectController {
   static createProject = async (req: Request, res: Response) => {
 
     const project = new Project(req.body)
+
+    project.manager = req.user.id
     try {
       await project.save()
       res.json({ msg: 'Proyecto Creado correctamente' })
