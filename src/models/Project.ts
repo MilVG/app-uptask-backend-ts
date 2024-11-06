be4c2609c0b3ac96b54f2e8fd0e73d8528736f01
notlim_dev <elmotigre6@gmail.com>
2024-11-06 17:01:26 -0500

    Incorporamos la realcion de usuarios para colaboradores en el modelos de Proyectos

--- src/models/Project.ts
+++ src/models/Project.ts
@@ -1,42 +1,52 @@
-import { Schema, model, Document, PopulatedDoc, Types } from "mongoose"
-import { ITask } from "./Task"
-import { IUser } from "./User"
+import { Schema, model, Document, PopulatedDoc, Types } from "mongoose";
+import { ITask } from "./Task";
+import { IUser } from "./User";
 
 export interface IProject extends Document {
-  projectName: string
-  clientName: string
-  description: string
-  tasks: PopulatedDoc<ITask & Document>[]
-  manager: PopulatedDoc<IUser & Document>
+  projectName: string;
+  clientName: string;
+  description: string;
+  tasks: PopulatedDoc<ITask & Document>[];
+  manager: PopulatedDoc<IUser & Document>;
+  team: PopulatedDoc<IUser & Document>[];
 }
 
-const ProjectSchema: Schema = new Schema({
-  projectName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  clientName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  tasks: [
-    {
+const ProjectSchema: Schema = new Schema(
+  {
+    projectName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    clientName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    tasks: [
+      {
+        type: Types.ObjectId,
+        ref: "Task",
+      },
+    ],
+    manager: {
       type: Types.ObjectId,
-      ref: 'Task'
-    }
-  ],
-  manager: {
-    type: Types.ObjectId,
-    ref: 'User'
-  }
-}, { timestamps: true })
+      ref: "User",
+    },
+    team: [
+      {
+        type: Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true },
+);
 
-const Project = model<IProject>("Project", ProjectSchema)
-export default Project
+const Project = model<IProject>("Project", ProjectSchema);
+export default Project;
