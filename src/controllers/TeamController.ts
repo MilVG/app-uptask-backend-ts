f7f567c73ee545e61a0b86245730e93f709e9822
notlim_dev <elmotigre6@gmail.com>
2024-11-06 21:20:09 -0500

    Implementacion de controller para agregar un miembro al equipo

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -13,5 +13,27 @@ export class TeamMemberController {
       return
     }
     res.json({ user: user })
-  }
+  };
+
+  static AddMemberById = async (req: Request, res: Response) => {
+
+    const { id } = req.body
+    const user = await User.findById(id).select('_id')
+    if (!user) {
+      const error = new Error('Usuario No Encontrado')
+      res.status(404).json({ error: error.message })
+      return
+    }
+
+    if (req.project.team.some(team => team.toString() === user.id.toString())) {
+      const error = new Error('Usuario ya existe en el Proyecto')
+      res.status(409).json({ error: error.message })
+      return
+
+    }
+    req.project.team.push(user.id)
+    await req.project.save()
+
+    res.json('usuario agregado correctamente')
+  };
 }
