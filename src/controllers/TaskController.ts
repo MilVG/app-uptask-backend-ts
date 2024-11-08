a05231a3fff064d1dc93ef454deda83e082feddf
notlim_dev <elmotigre6@gmail.com>
2024-11-08 01:21:47 -0500

    Incorporacion que usuario completó la tarea

--- src/controllers/TaskController.ts
+++ src/controllers/TaskController.ts
@@ -61,6 +61,11 @@ export class TaskController {
     try {
       const { status } = req.body
       req.task.status = status
+      if (status === 'pending') {
+        req.task.completedBy = null
+      } else {
+        req.task.completedBy = req.user.id
+      }
       await req.task.save()
       res.json({ msg: 'Status Tarea actualizada' })
 
