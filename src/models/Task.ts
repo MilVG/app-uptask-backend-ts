5c139eac205a2db6da16a892a60e53ade30c1460
notlim_dev <elmotigre6@gmail.com>
2024-11-08 01:21:23 -0500

    Modificacion del Modelo de tareas - incorporacion de quien completó la tarea

--- src/models/Task.ts
+++ src/models/Task.ts
@@ -17,6 +17,7 @@ export interface ITask extends Document {
   description: string
   project: Types.ObjectId
   status: TaskStatus
+  completedBy: Types.ObjectId
 }
 
 export const TaskSchema: Schema = new Schema({
@@ -38,6 +39,11 @@ export const TaskSchema: Schema = new Schema({
     type: String,
     enum: Object.values(taskStatus),
     default: taskStatus.PENDING
+  },
+  completedBy: {
+    type: Types.ObjectId,
+    ref: 'user',
+    default: null
   }
 }, { timestamps: true })
 
