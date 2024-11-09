221427c6bf45060bdde1bb9eeb2c99c7bb4c1ae5
notlim_dev <elmotigre6@gmail.com>
2024-11-09 14:27:29 -0500

    Controlador: Implementamos el codigo para crear una Nota.

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -4,5 +4,19 @@ import Note, { INote } from "../models/Note";
 export class NoteController {
   static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
     const { content } = req.body
+    const note = new Note()
+    note.content = content
+    note.createBy = req.user.id
+    note.task = req.task.id
+
+    req.task.notes.push(note.id)
+
+    try {
+
+      await Promise.allSettled([req.task.save(), note.save()])
+      res.json({ msg: 'Nota creada correctamente' })
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' })
+    }
   }
 }
