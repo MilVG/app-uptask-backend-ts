c11feba73b67cb2e169baca57b0a0cdf5dc242a4
notlim_dev <elmotigre6@gmail.com>
2024-11-09 12:07:09 -0500

    Definiendo tipado para Request con la interface INote

--- src/controllers/NoteController.ts
+++ src/controllers/NoteController.ts
@@ -2,7 +2,7 @@ import type { Request, Response } from "express";
 import Note, { INote } from "../models/Note";
 
 export class NoteController {
-  static createNote = async (req: Request, res: Response) => {
-    console.log(req.body)
+  static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
+    const { content } = req.body
   }
 }
